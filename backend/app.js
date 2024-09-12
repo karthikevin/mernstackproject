99e8eca920ce9a06dee46fa19b6e78adbf7e935d
karthikevin <161202034+karthikevin@users.noreply.github.com>
2024-09-13 01:09:35 +0530

    Update app.js
    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,14 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.get("/", (req,res)=>{
+    res.json({
+        success: true,
+        message: "welcome",
+     });
+});
+
+
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
